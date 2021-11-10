f3ed42c2d1c0ae0b10c11ffccf6350685b09ebce
Manohar Suman <msuman@anviam.com>
2021-11-11 01:11:38 +0530

    dependent-destroy

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,5 +2,5 @@ class Album < ApplicationRecord
     validates :title, presence: true
     validates :description, presence: true, length: { minimum: 7 }
 
-    has_many :images, dependent: :delete_all
+    has_many :images, dependent: :destroy
 end
