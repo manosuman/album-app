c013316a3f074ae1f09658d37cba23520d649bd3
Manohar Suman <msuman@anviam.com>
2021-11-18 13:18:38 +0530

    Changes

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,6 @@
 class Album < ApplicationRecord
     validates :title, presence: true
-    validates :description, presence: true, length: { minimum: 7 }
+    validates :description, presence: true
 
     has_many :images, dependent: :destroy
     belongs_to :user
